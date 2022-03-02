8b689959510c142686004e994365b4aac4b6329c
msisaif <msi132043@gmail.com>
2022-03-02 08:18:20 +0600

    Student Edit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -39,6 +39,114 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
+        $student = Student::create(
+            $this->validatedData($request)
+            + $this->storeGuardian($request)
+            + $this->storeAddress($request)
+        );
+
+        return redirect()
+            ->route('students.show', $student->id)
+            ->with('status', 'The record has been added successfully.');
+    }
+
+    public function show(Student $student)
+    {
+        return $this->formatedData($student);
+
+        return Inertia::render('Student/Show', [
+            'data' => [
+                'student' => $this->formatedData($student),
+            ]
+        ]);
+    }
+
+    public function edit(Student $student)
+    {
+        return Inertia::render('Student/Edit', [
+            'data' => $this->data($student)
+        ]);
+    }
+
+    public function update(Request $request, Student $student)
+    {
+        $student->update(
+            $this->validatedData($request, $student->id)
+            + $this->storeGuardian($request, $student)
+            + $this->storeAddress($request, $student)
+        );
+
+        return redirect()
+            ->route('students.show', $student->id)
+            ->with('status', 'The record has been update successfully.');
+    }
+
+    public function destroy(Student $student)
+    {
+        $student->delete();
+
+        return redirect()
+            ->route('students.index')
+            ->with('status', 'The record has been delete successfully.');
+    }
+
+    protected function data($student)
+    {
+        return [
+            'student'   => $this->formatedData($student),
+            'divisions' => DivisionResource::collection(Division::orderBy('name')->get()),
+            'districts' => DistrictResource::collection(District::orderBy('name')->get()),
+            'areas'     => AreaResource::collection(Area::orderBy('name')->get()),
+        ];
+    }
+
+    protected function formatedData($student)
+    {
+        StudentResource::withoutWrapping();
+
+        return new StudentResource(
+            $student->load([
+                'father_info',
+                'mother_info',
+                'guardian_info',
+                'present_address.area.district',
+                'permanent_address.area.district',
+            ]
+        ));
+    }
+
+    protected function getFilterProperty()
+    {
+        return [
+            //
+        ];
+    }
+
+    protected function validatedData($request, $id = '')
+    {
+        return $request->validate([
+            'name' => [
+                'required',
+                'string',
+            ],
+            'date_of_birth' => '',
+            'gender' => '',
+            'birth_certificate' => '',
+        ]);
+    }
+
+    protected function storeGuardian($request, $student = null)
+    {
+        if($student) {
+            Guardian::query()
+                ->whereIn('id', [
+                    $student->father_info_id,
+                    $student->mother_info_id,
+                    $student->guardian_info_id
+                ])
+                ->delete();
+        }
+
         $father_info_id = $this->storeGuardianGetId($request->father_info);
 
         $mother_info_id = $this->storeGuardianGetId($request->mother_info);
@@ -47,10 +155,28 @@ class StudentController extends Controller
             $guardian_info_id = $father_info_id;
         } 
         elseif ($request->guardian_type == 2) {
-            $guardian_info_id = $father_info_id;
+            $guardian_info_id = $mother_info_id;
         }
         else {
             $guardian_info_id = $this->storeGuardianGetId($request->guardian_info);
+        }
+
+        return [
+            'father_info_id'    => $father_info_id,
+            'mother_info_id'    => $mother_info_id,
+            'guardian_info_id'  => $guardian_info_id,
+        ];
+    }
+
+    protected function storeAddress($request, $student = null)
+    {
+        if($student) {
+            Address::query()
+                ->whereIn('id', [
+                    $student->present_address_id,
+                    $student->permanent_address_id
+                ])
+                ->delete();
         }
 
         $present_address_id = $this->storeAddressGetId($request->present_address);
@@ -59,30 +185,14 @@ class StudentController extends Controller
             ? $present_address_id
             : $this->storeAddressGetId($request->permanent_address);
 
-        $student = Student::create(
-            $this->validatedData($request) + [
-                'father_info_id'    => $father_info_id,
-                'mother_info_id'    => $mother_info_id,
-                'guardian_info_id'  => $guardian_info_id,
-            ] + [
-                'present_address_id'    => $present_address_id,
-                'permanent_address_id'  => $permanent_address_id,
-            ]
-        );
-
-        return $student;
-
-        return redirect()
-            ->route('students.show', $student->id)
-            ->with('status', 'The record has been added successfully.');
+        return [
+            'present_address_id'    => $present_address_id,
+            'permanent_address_id'  => $permanent_address_id,
+        ];
     }
 
     protected function storeGuardianGetId($guardian, $old_id = '')
     {
-        if($old_id) {
-            Guardian::where('id', $old_id)->delete();
-        }
-
         $response = Guardian::onlyTrashed()->updateOrCreate(
             [],
             [
@@ -113,116 +223,6 @@ class StudentController extends Controller
         );
 
         return $response->id ?? null;
-    }
-
-    public function show(Student $student)
-    {
-        return $this->GetStudentResourceData($student);
-
-        return Inertia::render('Student/Show', [
-            'data' => [
-                'student' => $this->GetStudentResourceData($student),
-            ]
-        ]);
-    }
-
-    public function edit(Student $student)
-    {
-        return Inertia::render('Student/Edit', [
-            'data' => $this->data($student)
-        ]);
-    }
-
-    public function update(Request $request, Student $student)
-    {
-        $father_info_id = $this->storeGuardianGetId($request->father_info, $student->father_info_id);
-
-        $mother_info_id = $this->storeGuardianGetId($request->mother_info, $student->mother_info_id);
-
-        if ($request->guardian_type == 1) {
-            $guardian_info_id = $father_info_id;
-        } 
-        elseif ($request->guardian_type == 2) {
-            $guardian_info_id = $father_info_id;
-        }
-        else {
-            $guardian_info_id = $this->storeGuardianGetId($request->guardian_info, $student->guardian_info_id);
-        }
-
-        $present_address_id = $this->storeAddressGetId($request->present_address, $student->present_address_id);
-
-        $permanent_address_id = $request->is_same_address
-            ? $present_address_id
-            : $this->storeAddressGetId($request->permanent_address, $student->permanent_address_id);
-
-        $student->update(
-            $this->validatedData($request, $student->id) + [
-                'father_info_id'    => $father_info_id,
-                'mother_info_id'    => $mother_info_id,
-                'guardian_info_id'  => $guardian_info_id,
-            ] + [
-                'present_address_id'    => $present_address_id,
-                'permanent_address_id'  => $permanent_address_id,
-            ]
-        );
-
-        return redirect()
-            ->route('students.show', $student->id)
-            ->with('status', 'The record has been update successfully.');
-    }
-
-    public function destroy(Student $student)
-    {
-        $student->delete();
-
-        return redirect()
-            ->route('students.index')
-            ->with('status', 'The record has been delete successfully.');
-    }
-
-    protected function data($student)
-    {
-        return [
-            'student'   => $this->GetStudentResourceData($student),
-            'divisions' => DivisionResource::collection(Division::orderBy('name')->get()),
-            'districts' => DistrictResource::collection(District::orderBy('name')->get()),
-            'areas'     => AreaResource::collection(Area::orderBy('name')->get()),
-        ];
-    }
-
-    protected function GetStudentResourceData($student)
-    {
-        StudentResource::withoutWrapping();
-
-        return new StudentResource(
-            $student->load([
-                'father_info',
-                'mother_info',
-                'guardian_info',
-                'present_address.area.district.division',
-                'permanent_address.area.district.division',
-            ]
-        ));
-    }
-
-    protected function getFilterProperty()
-    {
-        return [
-            //
-        ];
-    }
-
-    private function validatedData($request, $id = '')
-    {
-        return $request->validate([
-            'name' => [
-                'required',
-                'string',
-            ],
-            'date_of_birth' => '',
-            'gender' => '',
-            'birth_certificate' => '',
-        ]);
     }
 
 }

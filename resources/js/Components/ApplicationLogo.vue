35f6489a6d56d8ff96a24fcb302e74ddb268ba05
mahdihasan28 <mahdihasan28@gmail.com>
2024-06-02 10:11:58 +0600

    logo rounded

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,3 +1,3 @@
 <template>
-    <img class="bg-white rounded-full" :src="$page.props.settings.logoLink || '/images/logo.png'" alt="logo">
+    <img class="bg-white rounded-full p-1" :src="$page.props.settings.logoLink || '/images/logo.png'" alt="logo">
 </template>

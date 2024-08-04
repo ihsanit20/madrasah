a5ad4fa9919d2bf56a9fda4569abea683e8c966a
msisaif <msi132043@gmail.com>
2024-08-04 12:41:42 +0600

    build: static session 2024-25 for exam & set default image for student age 10 and more

---
+++ public/build/assets/TableTd-c6a42ad4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o,c as s,k as r}from"./app-815ecbc5.js";const a={},c={class:"whitespace-nowrap py-4 px-6 text-xs font-medium text-gray-900 dark:text-white md:text-sm"};function n(t,d){return o(),s("td",c,[r(t.$slots,"default")])}const p=e(a,[["render",n]]);export{p as T};
